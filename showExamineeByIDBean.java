eaef012499e20dee02e6e7cc58c47cb6900efa4e
wondercar <37956256+wondercar@users.noreply.github.com>
2018-05-15 21:00:52 +0800

    Create showExamineeByIDBean.java

---
+++ showExamineeByIDBean.java
@@ -0,0 +1,85 @@
+package mybean.data;
+
+public class showExamineeByIDBean {
+	private String examID = null;//身份证号
+	private String examName = null;//姓名
+	private String sex = null;//性别
+	private String examType = null;//考试类型
+	private String school = null;//工作地址
+	private String backMessage;//返回信息
+	
+	public String getExamID() {
+		return examID;
+	}
+	public void setExamID(String examID) {
+		this.examID = examID;
+	}
+	public String getExamName() {
+		return examName;
+	}
+	public void setExamName(String examName) {
+		this.examName = examName;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+//	public String getCompany() {
+//		return company;
+//	}
+//	public void setCompany(String company) {
+//		this.company = company;
+//	}
+//	public String getAddress() {
+//		return address;
+//	}
+//	public void setAddress(String address) {
+//		this.address = address;
+//	}
+//	public String getPhone() {
+//		return phone;
+//	}
+//	public void setPhone(String phone) {
+//		this.phone = phone;
+//	}
+//	public String getEmail() {
+//		return email;
+//	}
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+	public String getExamType() {
+		return examType;
+	}
+	public void setExamType(String examType) {
+		this.examType = examType;
+	}
+//	public String getMemo() {
+//		return memo;
+//	}
+//	public void setMemo(String memo) {
+//		this.memo = memo;
+//	}
+	public String getBackMessage() {
+		return backMessage;
+	}
+	public void setBackMessage(String backMessage) {
+		this.backMessage = backMessage;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+//	public String getPic() {
+//		return pic;
+//	}
+//	public void setPic(String pic) {
+//		this.pic = pic;
+//	}
+	
+	
+}
